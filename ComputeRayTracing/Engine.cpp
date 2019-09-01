1e7aacee91e55795e083c8cae9ac0d79d28c246c
Jamie Briggs <personal@jbriggs.dev>
2019-09-01 16:32:32 +0100

    Fixed light intensity.

--- ComputeRayTracing/Engine.cpp
+++ ComputeRayTracing/Engine.cpp
@@ -10,12 +10,12 @@ Engine::Engine()
 	one.SetColour(glm::vec3(0.0f, 1.0f, 0.0f));
 	m_spheres.push_back(one);
 	Sphere two;
-	two.SetPosition(glm::vec3(0.0f, 0.0f, -5.0f));
+	two.SetPosition(glm::vec3(0.0f, 0.0f, -5.5f));
 	two.SetRadius(1.5f);
 	two.SetColour(glm::vec3(0.0f, 0.0f, 1.0f));
 	m_spheres.push_back(two);
 	// Setup light
-	m_light = glm::vec3(-5.0f, -2.0f, 0.0f);
+	m_light = glm::vec3(-5.0f, 2.0f, 3.0f);
 }
 
 Engine::~Engine()
